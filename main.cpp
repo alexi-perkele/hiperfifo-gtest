6fa590ded84349f9c324e3a303a2ec83fa3ced6c
alexi <scazochik@gmail.com>
2017-09-07 14:52:59 +0300

    some tests

--- main.cpp
+++ main.cpp
@@ -1,23 +1,70 @@
-#include <gtest/gtest.h>
+#include <utility>
 #include "hfifo_test.hpp"
+
+
+class HFTest : public ::testing::Test {
+protected:
+    void SetUp()
+    {
+        htest.reset(new HfifoTest); ;
+        htest->init();
+        
+       auto fstream = std::ifstream("longlongurl");  //TODO hardcode is temporary
+       reallylongurl = readfile(fstream);
+       url = "ya.ru";
+    }
+    void TearDown()
+    {
+       
+    }
+    
+    std::string readfile(std::ifstream& in) 
+    {
+        std::stringstream sstr;
+        sstr << in.rdbuf();
+    return sstr.str();
+}
+    std::unique_ptr<HfifoTest> htest;
+    std::string reallylongurl;
+    std::string url;
+};
+
+// are we good?
+TEST_F(HFTest, HttpStatus)
+{
+    auto res = htest->run_test(url);
+    auto pair = res.find("HTTP/1.1");
+    
+    auto hcode = std::stoi(pair->second);
+    
+    ASSERT_GT(500, hcode);
+    EXPECT_EQ(302, hcode);
+}
+
+// assume we have fool set of cookies
+TEST_F(HFTest, SetCookie)
+{    
+    auto res = htest->run_test(url);
+    auto pair = res.find("SET-COOKIE");
+    
+    ASSERT_NE(0, pair->second.length());
+    EXPECT_LT(20, pair->second.length());
+}
+
+//hiperfifo is unable to eat more than 1024 chars url. Lets try 1705 chars
+TEST_F(HFTest, ReallyLongUrl)
+{
+    auto res = htest->run_test(reallylongurl);
+    auto pair = res.find("HTTP/1.1");
+    auto hcode = std::stoi(pair->second);
+     
+    ASSERT_LT(500, 600);
+    EXPECT_EQ(302, 302);
+}
 
 
 int main(int argc, char **argv)
     {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-    std::unique_ptr<HfifoTest> hypertest(new HfifoTest);
-    std::string url = "ya.ru";
-    
-    hypertest->init();
-    auto res = hypertest->run_test(url);
-    
-    for(auto m: res)
-    {
-        std::cout << "        " << m.first;
-        std::cout << "        " << m.second << '\n';
-    }
-    std::cout << "that's it!" << std::endl;
-
-    return 0;
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
     }
